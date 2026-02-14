667712820ede62867d2d1d856b53f0a5002f524f
AbhishekSoni1764 <abhisoni1764@gmail.com>
2026-02-14 17:14:42 +0530

    feat: Enhance Footer component with social links and add YouTube icon

--- apps/web/components/hero/Footer.tsx
+++ apps/web/components/hero/Footer.tsx
@@ -5,6 +5,97 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
+  const footLinks = [
+    { href: "/", label: "Home" },
+    { href: "/generate", label: "Generate Image" },
+    { href: "/#how-it-works", label: "How It Works" },
+    { href: "/gallery", label: "Gallery" },
+    { href: "/#guidelines", label: "Guidelines" },
+  ];
+  const legalLinks = [
+    {
+      href: "https://www.srmmilan.in/docs/privacy-policy.pdf",
+      label: "Privacy Policy",
+    },
+    {
+      href: "https://www.srmmilan.in/docs/terms-of-service.pdf",
+      label: "Terms & Conditions",
+    },
+    { href: "mailto:techteam.sa@srmist.edu.in", label: "Contact Us" },
+  ];
+  const aaruushSocials = [
+    {
+      href: "https://aaruush.org",
+      iconSrc: "/images/aarush_website.png",
+      alt: "Website",
+      size: 35,
+    },
+    {
+      href: "https://www.facebook.com/aaruush.srm",
+      iconSrc: "/images/facebook.png",
+      alt: "Facebook",
+      size: 18,
+    },
+    {
+      href: "https://twitter.com/aaruushsrmist",
+      iconSrc: "/images/X.png",
+      alt: "X",
+      size: 18,
+    },
+    {
+      href: "https://www.linkedin.com/company/aaruush-srm-ist/",
+      iconSrc: "/images/Linkedin.png",
+      alt: "LinkedIn",
+      size: 18,
+    },
+    {
+      href: "https://www.instagram.com/aaruush_srm/",
+      iconSrc: "/images/Instagram.png",
+      alt: "Instagram",
+      size: 18,
+    },
+    {
+      href: "https://play.google.com/store/apps/details?id=com.aaruush.connect",
+      iconSrc: "/images/Android.png",
+      alt: "Android",
+      size: 18,
+    },
+    {
+      href: "https://apps.apple.com/in/app/aaruush-connect/id6737286063",
+      iconSrc: "/images/ios.png",
+      alt: "iOS",
+      size: 18,
+    },
+  ];
+
+  const envisionSocials = [
+    {
+      href: "https://envision.aaruush.org",
+      iconSrc: "/images/Envision_website.png",
+      alt: "Website",
+    },
+    {
+      href: "https://www.facebook.com/teamenvision.srm/",
+      iconSrc: "/images/facebook.png",
+      alt: "Facebook",
+    },
+    {
+      href: "https://twitter.com/envision_srm",
+      iconSrc: "/images/X.png",
+      alt: "X",
+    },
+    {
+      href: "https://www.linkedin.com/company/team-envision/",
+      iconSrc: "/images/Linkedin.png",
+      alt: "LinkedIn",
+    },
+    {
+      href: "https://www.instagram.com/teamenvision_srm/",
+      iconSrc: "/images/Instagram.png",
+      alt: "Instagram",
+    },
+  ];
+
   return (
     <footer className="w-full flex flex-col justify-center bg-transparent relative z-10">
       <motion.div
@@ -17,17 +108,24 @@ export default function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-12 gap-12 lg:gap-8">
           <div className="md:col-span-3 flex flex-col items-center md:items-start justify-between space-y-6">
             <div className="flex flex-col items-center md:items-start w-full max-w-60">
-              <Image
-                src="/images/DSA_logo.png"
-                alt="Directorate of Student Affairs"
-                width={240}
-                height={240}
-                className="object-contain w-full h-auto"
-              />
+              <Link
+                href="https://www.srmmilan.in/"
+                target="_blank"
+                className="w-full"
+              >
+                <Image
+                  src="/images/DSA_logo.png"
+                  alt="Directorate of Student Affairs"
+                  width={240}
+                  height={240}
+                  className="object-contain w-full h-auto"
+                />
+              </Link>
 
               <div className="flex items-center justify-between w-full mt-8 md:mt-6 px-4 md:px-0">
                 <a
-                  href="#"
+                  href="https://www.facebook.com/srmmilan"
+                  target="_blank"
                   className="opacity-60 hover:opacity-100 transition-opacity"
                 >
                   <Image
@@ -39,7 +137,8 @@ export default function Footer() {
                   />
                 </a>
                 <a
-                  href="#"
+                  href="https://x.com/srm_milan"
+                  target="_blank"
                   className="opacity-60 hover:opacity-100 transition-opacity"
                 >
                   <Image
@@ -51,19 +150,21 @@ export default function Footer() {
                   />
                 </a>
                 <a
-                  href="#"
+                  href="https://www.youtube.com/@DSAEVENTS"
+                  target="_blank"
                   className="opacity-60 hover:opacity-100 transition-opacity"
                 >
                   <Image
-                    src="/images/Linkedin.png"
-                    alt="LinkedIn"
+                    src="/images/youtube.png"
+                    alt="Youtube"
                     width={24}
                     height={24}
-                    className="md:w-5 md:h-5"
+                    className="md:w-8 md:h-8"
                   />
                 </a>
                 <a
-                  href="#"
+                  href="https://www.instagram.com/srmist_milan/"
+                  target="_blank"
                   className="opacity-60 hover:opacity-100 transition-opacity"
                 >
                   <Image
@@ -84,19 +185,13 @@ export default function Footer() {
                 Quick Links
               </h4>
               <ul className="space-y-4">
-                {[
-                  "Home",
-                  "Generate Image",
-                  "How It Works",
-                  "Gallery",
-                  "Guidelines",
-                ].map((link) => (
-                  <li key={link}>
+                {footLinks.map((link) => (
+                  <li key={link.label}>
                     <Link
-                      href="#"
+                      href={link.href}
                       className="text-sm text-white/50 hover:text-white transition-colors block py-1 md:py-0"
                     >
-                      {link}
+                      {link.label}
                     </Link>
                   </li>
                 ))}
@@ -108,18 +203,14 @@ export default function Footer() {
                 Legal
               </h4>
               <ul className="space-y-4">
-                {[
-                  "Privacy Policy",
-                  "Terms & Conditions",
-                  "Support",
-                  "Contact Us",
-                ].map((link) => (
-                  <li key={link}>
+                {legalLinks.map((link) => (
+                  <li key={link.label}>
                     <Link
-                      href="#"
+                      href={link.href}
+                      target="_blank"
                       className="text-sm text-white/50 hover:text-white transition-colors block py-1 md:py-0"
                     >
-                      {link}
+                      {link.label}
                     </Link>
                   </li>
                 ))}
@@ -129,37 +220,33 @@ export default function Footer() {
 
           <div className="md:col-span-4 flex flex-col items-center md:items-end justify-between space-y-12 md:space-y-8 pt-8 md:pt-2">
             <div className="flex flex-col items-center md:items-end w-full max-w-[320px]">
-              <Image
-                src="/images/Aaruush_logo.png"
-                alt="Aaruush '25"
-                width={320}
-                height={80}
-                className="object-contain w-full h-auto"
-              />
+              <Link
+                href="https://www.aaruush.org/"
+                target="_blank"
+                className="w-full"
+              >
+                <Image
+                  src="/images/Aaruush_logo.png"
+                  alt="Aaruush '25"
+                  width={320}
+                  height={80}
+                  className="object-contain w-full h-auto"
+                />
+              </Link>
               <div className="flex items-center justify-between w-full mt-6 px-2 md:px-0">
-                {[
-                  {
-                    src: "/images/aarush_website.png",
-                    alt: "Website",
-                    w: 35,
-                  },
-                  { src: "/images/facebook.png", alt: "Facebook", w: 18 },
-                  { src: "/images/X.png", alt: "X", w: 18 },
-                  { src: "/images/Linkedin.png", alt: "LinkedIn", w: 18 },
-                  { src: "/images/Instagram.png", alt: "Instagram", w: 18 },
-                  { src: "/images/Android.png", alt: "Android", w: 18 },
-                  { src: "/images/ios.png", alt: "iOS", w: 18 },
-                ].map((icon, idx) => (
+                {aaruushSocials.map((social, idx) => (
                   <a
                     key={idx}
-                    href="#"
+                    href={social.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="opacity-60 hover:opacity-100 transition-opacity"
                   >
                     <Image
-                      src={icon.src}
-                      alt={icon.alt}
-                      width={icon.w}
-                      height={icon.w}
+                      src={social.iconSrc}
+                      alt={social.alt}
+                      width={social.size}
+                      height={social.size}
                       className="object-contain"
                     />
                   </a>
@@ -168,29 +255,31 @@ export default function Footer() {
             </div>
 
             <div className="flex flex-col items-center md:items-end w-full max-w-65">
-              <Image
-                src="/images/Envision_logo.png"
-                alt="Team Envision"
-                width={260}
-                height={90}
-                className="object-contain w-full h-auto"
-              />
+              <Link
+                href="https://envision.aaruush.org/"
+                target="_blank"
+                className="w-full"
+              >
+                <Image
+                  src="/images/Envision_logo.png"
+                  alt="Team Envision"
+                  width={260}
+                  height={90}
+                  className="object-contain w-full h-auto"
+                />
+              </Link>
               <div className="flex items-center justify-between w-full mt-6 px-2 md:px-0">
-                {[
-                  { src: "/images/Envision_website.png", alt: "Website" },
-                  { src: "/images/facebook.png", alt: "Facebook" },
-                  { src: "/images/X.png", alt: "X" },
-                  { src: "/images/Linkedin.png", alt: "LinkedIn" },
-                  { src: "/images/Instagram.png", alt: "Instagram" },
-                ].map((icon, idx) => (
+                {envisionSocials.map((social, idx) => (
                   <a
                     key={idx}
-                    href="#"
+                    href={social.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="opacity-60 hover:opacity-100 transition-opacity"
                   >
                     <Image
-                      src={icon.src}
-                      alt={icon.alt}
+                      src={social.iconSrc}
+                      alt={social.alt}
                       width={18}
                       height={18}
                       className="object-contain"
